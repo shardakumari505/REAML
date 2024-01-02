a9063f2f7a893f24d6c761061ef43a56d9599bb4
shardakumari505 <shardakumari505@gmail.com>
2024-01-03 02:25:35 +0530

    removed unused code from navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,5 @@
 import React from "react";
 import './navbar.styles.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { Link } from "react-router-dom";
 
 class Navbar extends React.Component {
